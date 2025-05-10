327aecfa66395095d44e7ff6f7c8493648e39824
bouakram <kikoua203@gmail.com>
2025-05-10 16:51:25 +0100

    made a mistake

--- .config/i3/monitorScrrenON.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-xrandr --output HDMI-1 --primary --auto --output eDP-1 --off
--- .config/polybar/launch_polybar.sh
+++
@@ -1,9 +0,0 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-    MONITOR=$m polybar --reload bottom &
-  done
-else
-  polybar --reload top &
-  polybar --reload bottom &
-fi
